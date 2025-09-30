551962163e4259a7b9c50590965b135311e8ff71
Greddi7 <tvilinev.artem@mail.ru>
2025-09-30 14:33:38 +0300

    Добавил интерфейс и работу с файлами

--- editor.py
+++ editor.py
@@ -1,8 +1,32 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
+
+def open_file():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+        text_area.delete(1.0, tk.END)
+        text_area.insert(1.0, content)
+
+def save_file():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt")
+    if file_path:
+        with open(file_path, 'w', encoding='utf-8') as file:
+            content = text_area.get(1.0, tk.END)
+            file.write(content)
 
 root = tk.Tk()
 root.title("Текстовый редактор")
+
+# Меню
+menu_bar = tk.Menu(root)
+file_menu = tk.Menu(menu_bar, tearoff=0)
+file_menu.add_command(label="Открыть", command=open_file)
+file_menu.add_command(label="Сохранить", command=save_file)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+root.config(menu=menu_bar)
 
 text_area = tk.Text(root)
 text_area.pack(fill=tk.BOTH, expand=1)
